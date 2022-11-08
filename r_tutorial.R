e4b00532c9b862296192c35114308752e4daf447
ramiadantsoa <tanjona.ramiadantsoa@gmail.com>
2022-11-08 11:49:18 +0300

    add new file

---
+++ r_tutorial.R
@@ -0,0 +1,37 @@
+1 + 1
+
+km <- 10 + 29 + 14 + 18
+
+kmjour <- km / 30
+ 1  2
+kmjour * 365
+
+km  <- "this is a text"
+ 1 + 2
+1 + "1"
+
+1 = 2
+1 == 2
+
+
+v1  <- c(1, 2, 3, 4)
+v2 <- 1:4
+v3 <- seq(1, 4, by = 1)
+
+v4 <- c()
+for(i in 1:4){
+    v4  <- c(v4, i)
+}
+
+v4
+m1 <- matrix(1:6, nrow = 2)
+
+name <- c("Tj", "To", "Ce", "Ka", "Ma", "SF", "Va")
+isFemale <- c(FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, FALSE)
+hairlength  <- runif(7, 0, 30)
+
+data <- data.frame(name, isFemale, hairlength)
+View(data)
+
+c(1, 2, 3, 4.2)
+
